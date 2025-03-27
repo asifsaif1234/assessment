d8368bebb64e48fd86c1407ffa157ea5eddaf8cc
asif saif <asifsaif786@gmail.com>
2025-03-27 16:52:15 +0530

    Adding logic and test cases for adding comma's and new line

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,10 +2,32 @@
 require_relative '../lib/calculator'
 
 RSpec.describe Calculator do
+
   describe '#add' do
-    it 'returns 0 for an empty string' do
+    it 'returns 0 for when passing empty string' do
       calculator = Calculator.new
       expect(calculator.add('')).to eq(0)
     end
+
+    it 'returns the number when a #single number string' do
+      calculator = Calculator.new
+      expect(calculator.add('1')).to eq(1)
+    end
+
+    it 'returns the sum for #two comma (,) separated numbers' do
+      calculator = Calculator.new
+      expect(calculator.add('1,5')).to eq(6)
+    end
+
+    it 'handles #any_amount of comma (,) separated numbers' do
+      calculator = Calculator.new
+      expect(calculator.add('1,2,3,4')).to eq(10)
+    end
+
+    it 'handles #new_lines between numbers (instead of commas).' do
+      calculator = Calculator.new
+      expect(calculator.add("1\n2,\n3")).to eq(6)
+    end
+
   end
 end
